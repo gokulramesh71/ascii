141b8abd2cfde353bfe99da5e87fa870d67697d1
gokulramesh71 <gramesh@softcell.com>
2019-10-30 14:23:50 +0530

    Name

---
+++ S1.java
@@ -0,0 +1,8 @@
+package com.softcell.mbx.workflow.connector;
+
+public class S1 {
+
+    public static void main(final String[] args) {
+        System.out.println((char) 3351 + "" + (char) 3403 + "" + (char) 3349 + "" + (char) 3393 + "" + (char) 3453);
+    }
+}
